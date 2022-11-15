0fd9cf3cd15e01b707cbe47d7b3018c72175589a
JaphethKunhiyop <japhethwaje@gmail.com>
2022-11-15 04:53:35 -0800

    Structure complete

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,80 +1,74 @@
+#include <stdlib.h>
 #include "dog.h"
-
-
-/**
- * _strlen - returns length of a string
- * @str: string to be counted
- * Return: returns length of string
- */
-int _strlen(char *str)
-{
-int len = 0;
-
-while (str)
-len++;
-
-return (len);
-}
-
+char *_strdup(char *str);
 
 /**
- * _strcopy - copy string pointed by src
- * into dest variable
- * @dest:buffer storing string copy
- * @src: buffer storing string to copy
- * Return:returns copied string
+ * new_dog - function with 3 arguments
+ * @name: char type pointer
+ * @age: float type
+ * @owner: char type pointer
+ * Description: creates a new dog
+ * Return: NULL if fail or pointer*
  */
-char *_strcopy(char *dest, char *src)
-{
-int index = 0;
 
-for (; src[index] ; index++)
-dest[index] = src[index];
-
-dest[index] = '\0';
-return (dest);
-}
-
-
-
-
-/**
- * new_dog - creates a new dog
- * @name: name of new dog
- * @age: age of new dog
- * @owner: owner of new dog
- * Return: returns NULL in case
- * of failure
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *doggo;
+	dog_t *new_d;
 
-if (name == NULL || age < 0 || owner == NULL)
-return (NULL);
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+		return (NULL);
 
-doggo = malloc(sizeof(dog_t));
-if (doggo == NULL)
-return (NULL);
+	new_d->name = _strdup(name);
+	if (!new_d->name)
+	{
+		free(new_d);
+		return (NULL);
+	}
+	new_d->age = age;
+	new_d->owner = _strdup(owner);
+	if (!new_d->owner)
+	{
+		free(new_d->name);
+		free(new_d);
+		return (NULL);
+	}
 
-doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (doggo->name == NULL)
-{
-free(doggo);
-return (NULL);
+	return (new_d);
+
 }
-doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+/**
+ *_strdup - function with one argument
+ * @str: string argument
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
 
-if (doggo->owner == NULL)
+char *_strdup(char *str)
+
 {
-free(doggo->name);
-free(doggo);
-return (NULL);
-}
+	int i, j;
+	char *ptr;
 
-doggo->name = _strcopy(doggo->name, name);
-doggo->age = age;
-doggo->owner = _strcopy(doggo->owner, owner);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
 
-return (doggo);
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
